529391f2fe530790e458cb65084bd5f8f0bdaa86
Alexey <valnistuy@gmail.com>
2020-04-14 00:01:17 +0300

    Updates

---
+++ precache-manifest.0f4e72b829eea70c482da1e048af421c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1faafb8a7e254faf6c61887cf2bb728c",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/index.html"
+  },
+  {
+    "revision": "731a0a6d03fb5a2cfc2a",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "81b71259fb890c0ea7bc",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/css/main.9ec3e329.chunk.css"
+  },
+  {
+    "revision": "731a0a6d03fb5a2cfc2a",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/2.3ab139b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/2.3ab139b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b71259fb890c0ea7bc",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/main.d955608d.chunk.js"
+  },
+  {
+    "revision": "3f863a18cd6ad4a17a7f",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/runtime-main.cbcde956.js"
+  }
+]);
